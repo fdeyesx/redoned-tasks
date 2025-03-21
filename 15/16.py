17f4522805a4871dea744cb8dfb10c0b47036883
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-21 16:33:45 +0300

    Create 16.py

---
+++ 15/16.py
@@ -0,0 +1,10 @@
+k = 0
+for a in range(0,500):
+    d = set()
+    for x in range(0,1000):
+        for y in range(0,1000):
+            f =  ((a < x) or (x**2 - 7*x + 10 > 0)) and ((a >= y) or (y**2 + 7*y + 12 >0))
+            d.add(f)
+    if False not in d:
+        k+=1
+print(k)
