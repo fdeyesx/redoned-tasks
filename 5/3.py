84fe375ab8ee0279ff749fc3950dd1f9fcdc209a
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-21 15:54:25 +0300

    Create 3.py

---
+++ 5/3.py
@@ -0,0 +1,12 @@
+def f(n):
+    s = str(bin(n))[2:]
+    if s.count('1')%2 == 0:
+        s = '10' + s[2:] +'0'
+    else:
+        s = '11' + s[2:] + '1'
+    return int(s,2)
+a = []
+for i in (27,100):
+    r = f(i)
+    a.append(r)
+print(min(a))
