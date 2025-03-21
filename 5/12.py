0f258bd2eae7b9723569c130a4836564e497acb4
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-21 16:10:19 +0300

    Create 12.py

---
+++ 5/12.py
@@ -0,0 +1,24 @@
+def k(n):
+    s = ''
+    while n!= 0:
+        s = str(n%3) + s
+        n //= 3
+    return s
+
+def f(n):
+    s = k(n)
+    sum1 = 0
+    for i in s:
+        sum1 += int(i)
+    if sum1%4 == 0:
+        s = '1' + s[:-2]
+    else:
+        s = s + k((sum1%4)*3)
+    return int(s,3)
+
+a = []
+for q in range(0,1000):
+    r = f(q)
+    if r > 353:
+        a.append(r)
+print(min(a))
