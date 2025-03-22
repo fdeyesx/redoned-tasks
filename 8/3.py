219c901ec352f4568aa52c4b4418e94a2f1c6d04
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-22 18:15:22 +0300

    Update 3.py

--- 8/3.py
+++ 8/3.py
@@ -2,7 +2,7 @@ from itertools import product, repeat
 k = 0
 for i in product('0123456789abcde',repeat=5):
     s = ''.join(i)
-    if s[0] != 0:
+    if s[0] != '0':
         if s.count('8') == 1:
             s = s.replace('b','a').replace('c','a').replace('d','a').replace('e','a')
             if s.count('a') > 1:
