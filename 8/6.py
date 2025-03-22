24fc5aa4d5ac9dad8fdc06562d48fe77d423fe72
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-22 18:18:29 +0300

    Update 6.py

--- 8/6.py
+++ 8/6.py
@@ -1,14 +1,13 @@
 from itertools import product, repeat
 r = 0
-for i in product('01234567',repeat=5):
+for i in product('01234567',repeat=7):
     s = ''.join(i)
     if s[0] != '0':
-        if s.count('1') == 0:
-            if len(set(s)) == len(s):
-                for j in '0246':
-                    s = s.replace(j,'+')
-                for k in '1357':
-                    s = s.replace(k, '-')
-                if '++' not in s or '--' not in s:
-                    r+=1
+        for j in '02468':
+            s = s.replace(j,'+')
+        if s.count('+') == 2:
+            for k in '135':
+                s = s.replace(k,'-')
+            if '-7' not in s and '7-' not in s:
+                r+=1
 print(r)
