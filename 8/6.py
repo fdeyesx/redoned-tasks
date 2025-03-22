dfa537fa15f4ff9ca43eee9d70327d0e8aab7d87
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-22 18:33:50 +0300

    Update 6.py

--- 8/6.py
+++ 8/6.py
@@ -6,8 +6,9 @@ for i in product('01234567',repeat=7):
         for j in '02468':
             s = s.replace(j,'+')
         if s.count('+') == 2:
-            for k in '135':
-                s = s.replace(k,'-')
-            if '-7' not in s and '7-' not in s:
-                r+=1
+            if '77' not in s:
+                for k in '135':
+                    s = s.replace(k,'-')
+                if '-7' not in s and '7-' not in s:
+                    r+=1
 print(r)
