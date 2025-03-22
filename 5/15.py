bacf26221c236f48d2e1b184346ab962edd025ee
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-22 18:08:08 +0300

    Update 15.py

--- 5/15.py
+++ 5/15.py
@@ -1,14 +1,19 @@
 def f(n):
     s = str(bin(n))[2:]
-    if s.count('1')%2 == 0:
-        s = s + '10'
-    else:
-        s = '11' + s
+    n1 = 0
+    while n1 < 2:
+        if s.count('1')%2 == 0:
+            s = s + '10'
+        else:
+            s = '11' + s
+        n1+=1
     return int(s,2)
 
 a = []
+k = 0
 for q in range(100,201):
     r = f(q)
     if r%2 == 0:
         a.append(r)
-print(len(a))
+        k+=1
+print(len(a),k)
