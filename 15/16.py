fe5d1751b53a11eeb3f1c64a2ac2577d44707c07
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-22 18:50:03 +0300

    Update 16.py

--- 15/16.py
+++ 15/16.py
@@ -1,8 +1,8 @@
 k = 0
-for a in range(0,500):
+for a in range(-500,500):
     d = set()
-    for x in range(0,1000):
-        for y in range(0,1000):
+    for x in range(-1000,1000):
+        for y in range(-1000,1000):
             f =  ((a < x) or (x**2 - 7*x + 10 > 0)) and ((a >= y) or (y**2 + 7*y + 12 >0))
             d.add(f)
     if False not in d:
