5b8ad2ea2cdeee17e01678dc790df806bf952de9
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-23 18:01:57 +0300

    Update 1.py

--- 17/1.py
+++ 17/1.py
@@ -1,1 +1,14 @@
+f = open('17.txt')
+a = []
+b = []
+k = 0
+for i in f:
+    a.append(int(i))
+for i in a:
+    if i % 9 != 0 and i % 11 != 0:
+        if i % 10 == 5 or i % 10 == 7:
+            k += 1
+            b.append(i)
 
+sum1 = min(b) + max(b)
+print(k, sum1)
