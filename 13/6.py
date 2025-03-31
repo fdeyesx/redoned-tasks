566206ea857d3563fa38149f9f2da56a0feb973c
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-31 17:10:40 +0300

    Create 6.py

---
+++ 13/6.py
@@ -0,0 +1,12 @@
+from ipaddress import ip_network
+for a in range(0,255+1):
+    n = ip_network(f'223.167.{a}.167/255.255.255.192',0)
+    k = 0
+    r = 0
+    for i in n:
+        r += 1
+        j = f'{i:b}'
+        if j[15:].count('1') >= j[:16].count('1'):
+            k+=1
+    if k == r:
+        print(a)
