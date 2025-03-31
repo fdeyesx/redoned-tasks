625d5e7289ab73ad0599edf82376c4a48bd8918c
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-31 17:17:23 +0300

    Update 6.py

--- 13/6.py
+++ 13/6.py
@@ -6,7 +6,7 @@ for a in range(0,255+1):
     for i in n:
         r += 1
         j = f'{i:b}'
-        if j[15:].count('1') >= j[:16].count('1'):
+        if j[16:].count('1') >= j[:16].count('1'):
             k+=1
     if k == r:
         print(a)
