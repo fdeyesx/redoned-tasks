f561b7f41434fda98c3dd36481aba052ac3b4dfd
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-03-31 16:48:50 +0300

    Update 1.py

--- 25.2/1.py
+++ 25.2/1.py
@@ -1,11 +1,11 @@
-for x in range(112_500_000,112_550_000):
+for x in range(112_500_000,112_550_000 + 1):
     d = []
     for i in range(2, int(x**0.5) + 1):
         if x % i == 0:
             d.append(i)
             d.append(x//i)
-    if len(d) != 0:
+    if len(d) >= 2:
         d = sorted(d)
         M = int(d[-1]) + int(d[-2])
-        if M%10000 == 1214:
+        if str(M)[-4:] == '1214':
             print(x)
