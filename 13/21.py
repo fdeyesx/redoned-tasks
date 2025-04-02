e375d1aad1f200c6583806ec9010ce1b68b235cc
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-04-02 18:49:43 +0300

    Create 21.py

---
+++ 13/21.py
@@ -0,0 +1,7 @@
+from ipaddress import ip_network
+n = ip_network('192.168.156.235/255.255.255.240',0)
+r = 1
+for i in n:
+    if str(i) == '192.168.156.235':
+        print(f'{r}. {i}')
+    r += 1
