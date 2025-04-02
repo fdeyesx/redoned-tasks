e31f485efd167db6f037781ad21546e599c41799
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-04-02 13:49:41 +0300

    Update 4.py

--- 25.2/4.py
+++ 25.2/4.py
@@ -1,9 +1,9 @@
 for x in range(1000,10000):
-    d = []
+    d = set()
     for i in range(1, int(x**0.5) + 1):
         if x % i == 0:
-            d.append(i)
-            d.append(x//i)
+            d.add(i)
+            d.add(x//i)
     if len(d) != 0:
         d = sorted(d)
         sum1 = 0
