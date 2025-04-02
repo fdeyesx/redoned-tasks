2677fbc8faec404435dbb61b78eae34200879734
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-04-02 13:49:15 +0300

    Update 14.py

--- 25.2/14.py
+++ 25.2/14.py
@@ -1,21 +1,21 @@
 def f(n):
     r = []
     for j in n:
-        k = []
+        k = set()
         for i in range(2, int(x ** 0.5) + 1):
             if j % i == 0:
-                r.append(i)
-                r.append(j // i)
+                k.add(i)
+                k.add(j // i)
         if len(k) == 2:
             r.append(j)
     return r
 
 for x in range(670_000,770_000):
-    d = []
+    d = set()
     for i in range(2, int(x**0.5) + 1):
         if x % i == 0:
-            d.append(i)
-            d.append(x//i)
+            d.add(i)
+            d.add(x//i)
     if len(d) != 0:
         sum1 = 0
         k =f(d)
