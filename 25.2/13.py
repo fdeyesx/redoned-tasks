576247339b1af1d26b6d6647f798b59c55d4d767
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-04-02 13:47:58 +0300

    Update 13.py

--- 25.2/13.py
+++ 25.2/13.py
@@ -1,10 +1,10 @@
 k = 0
 for x in range(800_000,10**20):
-    d = []
+    d = set()
     for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
-            d.append(i)
-            d.append(x // i)
+            d.add(i)
+            d.add(x // i)
     if len(d) > 0:
         m = min(d) + max(d)
         if m%138 == 0:
