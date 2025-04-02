7daab3e83a21323ff463725a0e04f47274939efe
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-04-02 13:47:21 +0300

    Update 1.py

--- 25.2/1.py
+++ 25.2/1.py
@@ -1,9 +1,9 @@
 for x in range(112_500_000,112_550_000 + 1):
-    d = []
+    d = set()
     for i in range(2, int(x**0.5) + 1):
         if x % i == 0:
-            d.append(i)
-            d.append(x//i)
+            d.add(i)
+            d.add(x//i)
     if len(d) >= 2:
         d = sorted(d)
         M = int(d[-1]) + int(d[-2])
