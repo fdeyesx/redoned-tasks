8592957827a54ec0f84f3b9b27805a0d3f196cb3
冰道。| Ashley. <118361641+fdeyesx@users.noreply.github.com>
2025-04-02 18:54:16 +0300

    Update 17.py

--- 13/17.py
+++ 13/17.py
@@ -1,7 +1,6 @@
 from ipaddress import ip_network
 n = ip_network('156.132.15.138/255.255.252.0',0)
-a = n.network_address
-r = 1
+r = 0
 for i in n:
     if str(i) == '156.132.15.138':
         print(f'{r}. {i}')
